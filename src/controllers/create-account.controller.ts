b0158a1b60e0bf6135b9095cdbb364d42cc4d000
eduardobertozi <webdu15@gmail.com>
2025-02-17 09:03:35 -0300

    feat: add CreateAccountController for user account creation with email conflict handling

---
+++ src/controllers/create-account.controller.ts
@@ -0,0 +1,31 @@
+import { ConflictException } from '@nestjs/common'
+import { Body, Controller, Post } from '@nestjs/common'
+import { PrismaService } from 'src/prisma/prisma.service'
+
+@Controller('/accounts')
+export class CreateAccountController {
+  constructor(private readonly prisma: PrismaService) {}
+
+  @Post()
+  async handle(@Body() body: any) {
+    const { name, email, password } = body
+
+    const userWithSameEmail = await this.prisma.user.findUnique({
+      where: {
+        email,
+      },
+    })
+
+    if (userWithSameEmail) {
+      throw new ConflictException('User with this email already exists')
+    }
+
+    await this.prisma.user.create({
+      data: {
+        name,
+        email,
+        password,
+      },
+    })
+  }
+}
