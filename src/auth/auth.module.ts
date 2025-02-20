5dfb664472b3fb1d05febe324c1ac413157b9327
eduardobertozi <webdu15@gmail.com>
2025-02-20 09:01:39 -0300

    feat: implement JWT authentication with Passport strategy and add CreateQuestionController

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,6 +3,7 @@ import { PassportModule } from '@nestjs/passport'
 import { JwtModule } from '@nestjs/jwt'
 import { ConfigService } from '@nestjs/config'
 import { Env } from 'src/env'
+import { JwtStrategy } from './strategy'
 
 @Module({
   imports: [
@@ -13,13 +14,16 @@ import { Env } from 'src/env'
       useFactory(configService: ConfigService<Env, true>) {
         const privateKey = configService.get('JWT_PRIVATE_KEY', { infer: true })
         const publicKey = configService.get('JWT_PUBLIC_KEY', { infer: true })
+
         return {
           signOptions: { algorithm: 'RS256' },
           privateKey: Buffer.from(privateKey, 'base64'),
           publicKey: Buffer.from(publicKey, 'base64'),
+          // secret: configService.get('JWT_SECRET', { infer: true }),
         }
       },
     }),
   ],
+  providers: [JwtStrategy],
 })
 export class AuthModule {}
