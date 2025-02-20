30825643fd57cdc1a510551e87f2b29338f48eba
eduardobertozi <webdu15@gmail.com>
2025-02-20 11:47:23 -0300

    feat: add FetchRecentQuestionsController with pagination and Zod validation for query parameters

--- src/pipes/zod-validation.pipe.ts
+++ src/pipes/zod-validation.pipe.ts
@@ -3,7 +3,7 @@ import { ZodError, ZodSchema } from 'zod'
 import { fromZodError } from 'zod-validation-error'
 
 export class ZodValidationPipe implements PipeTransform {
-  constructor(private schema: ZodSchema) {}
+  constructor(private readonly schema: ZodSchema) {}
 
   transform(value: unknown) {
     try {
