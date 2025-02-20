e07900764d3d378e32991e3f822f2c028e0d5dd8
eduardobertozi <webdu15@gmail.com>
2025-02-20 15:30:18 -0300

    feat: add Prisma basic repositories for questions, answers, and comments to enhance data management

--- src/infra/database/database.module.ts
+++ src/infra/database/database.module.ts
@@ -1,8 +1,30 @@
 import { Module } from '@nestjs/common'
 import { PrismaService } from './prisma/prisma.service'
+import { PrismaQuestionsRepository } from './prisma/repositories/prisma-questions.reporitory'
+import { PrismaQuestionCommentsRepository } from './prisma/repositories/prisma-question-comments.repository'
+import { PrismaQuestionAttatchmentsRepository } from './prisma/repositories/prisma-question-attachments.repository'
+import { PrismaAnswersRepository } from './prisma/repositories/prisma-answers.repository'
+import { PrismaAnswerCommentsRepository } from './prisma/repositories/prisma-answer-comments.repository'
+import { PrismaAnswerAttatchmentsRepository } from './prisma/repositories/prisma-answer-attachments.repository'
 
 @Module({
-  providers: [PrismaService],
-  exports: [PrismaService],
+  providers: [
+    PrismaService,
+    PrismaQuestionsRepository,
+    PrismaQuestionCommentsRepository,
+    PrismaQuestionAttatchmentsRepository,
+    PrismaAnswersRepository,
+    PrismaAnswerCommentsRepository,
+    PrismaAnswerAttatchmentsRepository,
+  ],
+  exports: [
+    PrismaService,
+    PrismaQuestionsRepository,
+    PrismaQuestionCommentsRepository,
+    PrismaQuestionAttatchmentsRepository,
+    PrismaAnswersRepository,
+    PrismaAnswerCommentsRepository,
+    PrismaAnswerAttatchmentsRepository,
+  ],
 })
 export class DatabaseModule {}
