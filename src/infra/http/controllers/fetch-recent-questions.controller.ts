adf458fa3c45a7425ddb0c5c78454ab48af74bc1
eduardobertozi <webdu15@gmail.com>
2025-02-22 14:24:10 -0300

    feat: refactor FetchRecentQuestionsController to use FetchRecentQuestionsUseCase and implement QuestionPresenter for response mapping

--- src/infra/http/controllers/fetch-recent-questions.controller.ts
+++ src/infra/http/controllers/fetch-recent-questions.controller.ts
@@ -1,7 +1,8 @@
-import { Controller, Get, Query, UseGuards } from '@nestjs/common'
+import { FetchRecentQuestionsUseCase } from '@/domain/forum/application/use-cases/fetch-recent-questions'
 import { JwtAuthGuard } from '@/infra/auth/jwt-auth.guard'
 import { ZodValidationPipe } from '@/infra/http/pipes/zod-validation.pipe'
-import { PrismaService } from '@/infra/database/prisma/prisma.service'
+import { QuestionPresenter } from '@/infra/presenters/http-questions.presenter'
+import { Controller, Get, Query, UseGuards } from '@nestjs/common'
 import { z } from 'zod'
 
 const pageQueryParamSchema = z
@@ -18,20 +19,26 @@ type PageQueryParamSchema = z.infer<typeof pageQueryParamSchema>
 @Controller('/questions')
 @UseGuards(JwtAuthGuard)
 export class FetchRecentQuestionsController {
-  constructor(private readonly prisma: PrismaService) {}
+  constructor(
+    private readonly fetchRecentQuestions: FetchRecentQuestionsUseCase,
+  ) {}
 
   @Get()
   async handle(@Query('page', queryValidationPipe) page: PageQueryParamSchema) {
-    const perPage = 20
-
-    const questions = await this.prisma.question.findMany({
-      take: perPage,
-      skip: (page - 1) * perPage,
-      orderBy: {
-        createdAt: 'desc',
-      },
+    const result = await this.fetchRecentQuestions.execute({
+      page,
     })
 
-    return { questions }
+    if (result.isLeft()) {
+      throw new Error()
+    }
+
+    const questions = result.value.questions
+
+    return {
+      questions: questions.map((question) =>
+        QuestionPresenter.toHTTP(question),
+      ),
+    }
   }
 }
--- src/infra/http/http.module.ts
+++ src/infra/http/http.module.ts
@@ -1,17 +1,17 @@
 import { Module } from '@nestjs/common'
 
+import { ChooseQuestionBestAnswerUseCase } from '@/domain/forum/application/use-cases/choose-question-best-answer'
+import { CommentOnAnswerUseCase } from '@/domain/forum/application/use-cases/comment-on-answer'
+import { CommentOnQuestionUseCase } from '@/domain/forum/application/use-cases/comment-on-question'
+import { CreateQuestionUseCase } from '@/domain/forum/application/use-cases/create-question'
+import { DeleteAnswerCommentUseCase } from '@/domain/forum/application/use-cases/delete-answer-comment'
+import { DeleteQuestionCommentUseCase } from '@/domain/forum/application/use-cases/delete-question-comment'
+import { FetchRecentQuestionsUseCase } from '@/domain/forum/application/use-cases/fetch-recent-questions'
+import { DatabaseModule } from '../database/database.module'
 import { AuthenticateController } from './controllers/authenticate.controller'
 import { CreateAccountController } from './controllers/create-account.controller'
 import { CreateQuestionController } from './controllers/create-question.controller'
 import { FetchRecentQuestionsController } from './controllers/fetch-recent-questions.controller'
-import { DatabaseModule } from '../database/database.module'
-import { CreateQuestionUseCase } from '@/domain/forum/application/use-cases/create-question'
-import { DeleteQuestionCommentUseCase } from '@/domain/forum/application/use-cases/delete-question-comment'
-import { DeleteAnswerCommentUseCase } from '@/domain/forum/application/use-cases/delete-answer-comment'
-import { ChooseQuestionBestAnswerUseCase } from '@/domain/forum/application/use-cases/choose-question-best-answer'
-import { FetchAnswerCommentsUseCase } from '@/domain/forum/application/use-cases/fetch-answer-comments'
-import { CommentOnQuestionUseCase } from '@/domain/forum/application/use-cases/comment-on-question'
-import { CommentOnAnswerUseCase } from '@/domain/forum/application/use-cases/comment-on-answer'
 
 @Module({
   imports: [DatabaseModule],
@@ -26,7 +26,7 @@ import { CommentOnAnswerUseCase } from '@/domain/forum/application/use-cases/com
     DeleteQuestionCommentUseCase,
     DeleteAnswerCommentUseCase,
     ChooseQuestionBestAnswerUseCase,
-    FetchAnswerCommentsUseCase,
+    FetchRecentQuestionsUseCase,
     CommentOnQuestionUseCase,
     CommentOnAnswerUseCase,
   ],
