198adb05777128b5b7c5a1bda0b05f3a62030934
eduardobertozi <webdu15@gmail.com>
2025-02-25 22:49:01 -0300

    feat: add factories for creating answer and question comments with Prisma integration

--- test/factories/make-answer-comment.ts
+++ test/factories/make-answer-comment.ts
@@ -4,6 +4,9 @@ import {
   AnswerComment,
   AnswerCommentProps,
 } from '@/domain/forum/enterprise/entities/answer-comment'
+import { Injectable } from '@nestjs/common'
+import { PrismaService } from '@/infra/database/prisma/prisma.service'
+import { PrismaAnswerCommentMapper } from '@/infra/database/prisma/mappers/prisma-answer-comment.mapper'
 
 export function makeAnswerComment(
   override: Partial<AnswerCommentProps> = {},
@@ -18,4 +21,21 @@ export function makeAnswerComment(
     },
     id,
   )
+}
+
+@Injectable()
+export class AnswerCommentFactory {
+  constructor(private readonly prismaService: PrismaService) {}
+
+  async makePrismaAnswer(
+    data: Partial<AnswerCommentProps> = {},
+  ): Promise<AnswerComment> {
+    const answerComment = makeAnswerComment(data)
+
+    await this.prismaService.comment.create({
+      data: PrismaAnswerCommentMapper.toPrisma(answerComment),
+    })
+
+    return answerComment
+  }
 }
