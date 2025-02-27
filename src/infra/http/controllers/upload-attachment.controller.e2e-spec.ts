ea6230efffbe0dd7c5b7ade5bb928c0a0076995a
eduardobertozi <webdu15@gmail.com>
2025-02-27 13:45:31 -0300

    feat: implement UploadAttachments controller with E2E tests and add multer types

---
+++ src/infra/http/controllers/upload-attachment.controller.e2e-spec.ts
@@ -0,0 +1,39 @@
+import { AppModule } from '@/infra/app.module'
+import { DatabaseModule } from '@/infra/database/database.module'
+import { INestApplication } from '@nestjs/common'
+import { JwtService } from '@nestjs/jwt'
+import { Test } from '@nestjs/testing'
+import { StudentFactory } from '@root/test/factories/make-student'
+import request from 'supertest'
+
+describe('Upload Attachment [E2E]', () => {
+  let app: INestApplication
+  let studentFactory: StudentFactory
+  let jwt: JwtService
+
+  beforeEach(async () => {
+    const moduleRef = await Test.createTestingModule({
+      imports: [AppModule, DatabaseModule],
+      providers: [StudentFactory],
+    }).compile()
+
+    app = moduleRef.createNestApplication()
+    studentFactory = moduleRef.get(StudentFactory)
+    jwt = moduleRef.get(JwtService)
+
+    await app.init()
+  })
+
+  test('[POST] /attachments', async () => {
+    const user = await studentFactory.makePrismaStudent()
+
+    const access_token = jwt.sign({ sub: user.id.toString() })
+
+    const response = await request(app.getHttpServer())
+      .post(`/attachments`)
+      .set('Authorization', `Bearer ${access_token}`)
+      .attach('file', './test/e2e/sample.jpg')
+
+    expect(response.statusCode).toBe(201)
+  })
+})
---
+++ src/infra/http/controllers/upload-attachment.controller.ts
@@ -0,0 +1,31 @@
+import {
+  Controller,
+  MaxFileSizeValidator,
+  ParseFilePipe,
+  Post,
+  UploadedFile,
+  UseInterceptors,
+} from '@nestjs/common'
+import { FileInterceptor } from '@nestjs/platform-express'
+
+@Controller('/attachments')
+export class UploadAttachmentsController {
+  constructor() {}
+
+  @Post()
+  @UseInterceptors(FileInterceptor('file'))
+  async handle(
+    @UploadedFile(
+      new ParseFilePipe({
+        validators: [
+          new MaxFileSizeValidator({
+            maxSize: 2 * 1024 * 1024,
+          }),
+        ],
+      }),
+    )
+    file: Express.Multer.File,
+  ) {
+    console.log(file)
+  }
+}
